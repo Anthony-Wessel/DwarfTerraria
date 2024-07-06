fae5f55e273711e81dd21209608aec3032168a2e
Anthony <60476666+Anthony-Wessel@users.noreply.github.com>
2024-07-06 15:44:14 -0500

    Implemented random cave entrance generation

--- environment/procedural generation/WorldGenerator.gd
+++ environment/procedural generation/WorldGenerator.gd
@@ -49,7 +49,7 @@ static func GenerateWorld(worldResource : GameSave):
 	var mid_chunk : int = worldResource.world_info.size.x / 2
 	var chunk = worldResource.get_chunk(Vector2(mid_chunk, 2))
 	@warning_ignore("integer_division")
-	var t = chunk.tiles[GlobalReferences.CHUNK_SIZE/2] # 0 for tiles, then x value (y = 0)
+	var t = chunk.tiles[GlobalReferences.CHUNK_SIZE/2] # (y = 0)
 	var spawn_y = 0
 	while t != grass:
 		spawn_y += 1
@@ -60,51 +60,17 @@ static func GenerateWorld(worldResource : GameSave):
 	var spawnY = GlobalReferences.CHUNK_SIZE * 2 + spawn_y-1
 	worldResource.world_info.player_spawn = Vector2(spawnX, spawnY)
 	
+	
+	place_caves(worldResource)
+	place_trees(worldResource)
+	
+	
 	# calculate lighting
 	LightManager.preload_lighting(worldResource)
 	
-	place_trees(worldResource)
-
-static func place_trees(worldResource : GameSave):
-	var just_placed = false
-	for x in range(1, worldResource.get_width()-1):
-		if rng.randf() < 0.8 or just_placed:
-			just_placed = false
-			continue
-		
-		just_placed = true
-		
-		# find the highest solid tile
-		var tile = worldResource.get_tile(Vector2i(x, 2*GlobalReferences.CHUNK_SIZE))
-		var y = 2*GlobalReferences.CHUNK_SIZE
-		while tile.id == empty and y < (3*GlobalReferences.CHUNK_SIZE) - 1:
-			y += 1
-			tile = worldResource.get_tile(Vector2i(x, y))
-		if y >= (3*GlobalReferences.CHUNK_SIZE) - 1:
-			continue
-		
-		# place the tree
-		worldResource.set_tile(Vector2i(x, y-1), tree_base)
-		var y2 = y-1
-		for i in rng.randi_range(3,10):
-			y2 -= 1 
-			var r = rng.randf()
-			if r < 0.1 and worldResource.get_tile(Vector2i(x-1, y2)).id == empty:
-				worldResource.set_tile(Vector2i(x, y2), tree_trunk_left)
-				worldResource.set_tile(Vector2i(x-1, y2), tree_branch_left)
-			elif r > 0.9 and worldResource.get_tile(Vector2i(x+1, y2)).id == empty:
-				worldResource.set_tile(Vector2i(x, y2), tree_trunk_right)
-				worldResource.set_tile(Vector2i(x+1, y2), tree_branch_right)
-			else:
-				worldResource.set_tile(Vector2i(x, y2), tree_trunk)
-		
-		worldResource.set_tile(Vector2i(x, y2-1), tree_top5)
-		worldResource.set_tile(Vector2i(x-1, y2-1), tree_top4)
-		worldResource.set_tile(Vector2i(x+1, y2-1), tree_top6)
-		worldResource.set_tile(Vector2i(x, y2-2), tree_top2)
-		worldResource.set_tile(Vector2i(x-1, y2-2), tree_top1)
-		worldResource.set_tile(Vector2i(x+1, y2-2), tree_top3)
-
+############################
+# Initial chunk generation #
+############################
 static func generate_cave_texture(rng_seed : int, offset : Vector2) -> Texture2D:
 	var cave_tex = await generate_cave_tex(rng_seed, offset)
 	var refined_cave_tex = ShaderComputer.run_shader("res://environment/procedural generation/procedural_terrain.glsl", [cave_tex], GlobalReferences.CHUNK_SIZE, GlobalReferences.CHUNK_SIZE)
@@ -253,3 +219,99 @@ static func generate_cave_chunk(chunk_coords: Vector2, rng_seed : int):
 			lights.append(Vector2(0,0))
 	
 	return [tiles, walls, lights]
+
+###################
+# Surface Editing #
+###################
+static func place_trees(worldResource : GameSave):
+	var just_placed = false
+	for x in range(1, worldResource.get_width()-1):
+		if rng.randf() < 0.8 or just_placed:
+			just_placed = false
+			continue
+		
+		just_placed = true
+		
+		# find the highest solid tile
+		var tile = worldResource.get_tile(Vector2i(x, 2*GlobalReferences.CHUNK_SIZE))
+		var y = 2*GlobalReferences.CHUNK_SIZE
+		while tile.id != grass and y < (3*GlobalReferences.CHUNK_SIZE) - 1:
+			y += 1
+			tile = worldResource.get_tile(Vector2i(x, y))
+		if y >= (3*GlobalReferences.CHUNK_SIZE) - 1:
+			continue
+		
+		# place the tree
+		worldResource.set_tile(Vector2i(x, y-1), tree_base)
+		var y2 = y-1
+		for i in rng.randi_range(3,10):
+			y2 -= 1 
+			var r = rng.randf()
+			if r < 0.1 and worldResource.get_tile(Vector2i(x-1, y2)).id == empty:
+				worldResource.set_tile(Vector2i(x, y2), tree_trunk_left)
+				worldResource.set_tile(Vector2i(x-1, y2), tree_branch_left)
+			elif r > 0.9 and worldResource.get_tile(Vector2i(x+1, y2)).id == empty:
+				worldResource.set_tile(Vector2i(x, y2), tree_trunk_right)
+				worldResource.set_tile(Vector2i(x+1, y2), tree_branch_right)
+			else:
+				worldResource.set_tile(Vector2i(x, y2), tree_trunk)
+		
+		worldResource.set_tile(Vector2i(x, y2-1), tree_top5)
+		worldResource.set_tile(Vector2i(x-1, y2-1), tree_top4)
+		worldResource.set_tile(Vector2i(x+1, y2-1), tree_top6)
+		worldResource.set_tile(Vector2i(x, y2-2), tree_top2)
+		worldResource.set_tile(Vector2i(x-1, y2-2), tree_top1)
+		worldResource.set_tile(Vector2i(x+1, y2-2), tree_top3)
+
+static func place_caves(worldResource : GameSave):
+	var mid_chunk : int = worldResource.world_info.size.x / 2
+	var cooldown = 0
+	for x in worldResource.world_info.size.x:
+		if cooldown > 0:
+			cooldown -= 1
+			continue
+		if x == mid_chunk:
+			continue
+		
+		if rng.randf() > 0.2:
+			dig_cave(worldResource, Vector2(x,2), Vector2(sign(rng.randf()-0.5),0))
+			cooldown = 2
+
+static func dig_cave(worldResource : GameSave, chunk_coords : Vector2, direction : Vector2):
+	# Find the starting point for the cave on the surface
+	var surface_center = (chunk_coords + Vector2(0.5, 0)) * GlobalReferences.CHUNK_SIZE
+	var counter = 0
+	while worldResource.get_tile(surface_center) == TileHandler.EMPTY_TILE and counter < GlobalReferences.CHUNK_SIZE:
+		surface_center += Vector2(0,1)
+		counter += 1
+	
+	surface_center += Vector2(0,-3)
+	
+	var depth = 40
+	var offset = Vector2(0,0)
+	for i in depth:
+		var horizontal = rng.randi_range(0,4)-2
+		if horizontal < 0: horizontal = 0
+		for x in horizontal:
+			erase_tiles_x(worldResource, surface_center + offset, 5)
+			offset += direction
+			
+		erase_tiles_x(worldResource, surface_center + offset, 5)
+		offset += Vector2(0,1)
+	
+	erase_tiles_circle(worldResource, surface_center + offset, 5)
+	
+
+static func erase_tiles_x(worldResource:  GameSave, center : Vector2, radius : int):
+	for i in radius:
+		worldResource.set_tile(center + i*Vector2(0,1), empty)
+		worldResource.set_tile(center + i*Vector2(0,-1), empty)
+		worldResource.set_tile(center + i*Vector2(1,0), empty)
+		worldResource.set_tile(center + i*Vector2(-1,0), empty)
+
+static func erase_tiles_circle(worldResource : GameSave, center : Vector2, radius : int):
+	for x in range(2*radius+1):
+		for y in range(2*radius+1):
+			var offset = Vector2(x,y) - Vector2.ONE * radius
+			if offset.length() < radius:
+				worldResource.set_tile(center+offset, empty)
