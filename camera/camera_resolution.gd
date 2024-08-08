45e88520d7adcc6ceb4f9c05b511b0a1671d372b
Anthony <60476666+Anthony-Wessel@users.noreply.github.com>
2024-08-07 21:21:52 -0500

    UI properly scales when window size changes

--- camera/camera_resolution.gd
+++ camera/camera_resolution.gd
@@ -3,6 +3,11 @@ extends Camera2D
 @export var target_resolution : Vector2i
 
 func _ready():
+	pass
+	resize()
+	get_tree().get_root().size_changed.connect(resize)
+
+func resize():
 	var viewport_resolution = get_viewport_rect().size
 	zoom.x = viewport_resolution.x/target_resolution.x
 	zoom.y = viewport_resolution.y/target_resolution.y
